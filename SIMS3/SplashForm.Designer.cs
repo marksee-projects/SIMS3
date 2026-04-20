95b9c08cacef2bc2ba40047edcf327c7710e3785
Mark Antonie See <markseejr@gmail.com>
2026-04-21 00:19:13 +0800

    added a splash form

---
+++ SIMS3/SplashForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SIMS3
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashForm));
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1000, 597);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.ForeColor = Color.RoyalBlue;
+            progressBar1.Location = new Point(259, 361);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(504, 33);
+            progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(323, 273);
+            label1.Name = "label1";
+            label1.Size = new Size(372, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Bataan Peninsula State University";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Black;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(434, 96);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(127, 162);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Black;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(383, 304);
+            label2.Name = "label2";
+            label2.Size = new Size(247, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Information Management";
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 40;
+            timer2.Tick += timer2_Tick;
+            // 
+            // SplashForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1000, 597);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "SplashForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SplashForm";
+            Load += SplashForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
